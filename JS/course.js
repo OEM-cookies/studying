30539d8af1a9c8691cb061e3aaadfa19f944520e
Tim <example@gmail.com>
2020-11-12 18:18:41 +0300

    added switch case

--- JS/course.js
+++ JS/course.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const num = 50;
+
+switch (num) {
+	case 49:
+		console.log('Wrong');
+		break;
+	case 100:
+		console.log('Wrong');
+		break;
+	case 50:
+		console.log('Right!');
+		break;
+	default:
+		console.log('Not now');
+		break;
+}
