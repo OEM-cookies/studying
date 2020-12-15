66fdce2397af05f0b65f11c3008e894409b9c7f7
koti <koti@mworx.ru>
2020-12-15 17:08:23 +0300

    ended capter22

--- JS/ep22.js
+++ JS/ep22.js
@@ -48,3 +48,30 @@ newNumbers.c.x = 10;
 
 console.log(newNumbers);
 console.log(numbers);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+	blogs = ['wordpress', 'livejournal','blogger'],
+	internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+const num = [2, 5 , 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newArray = [...array];
+
+const q = {
+	one: 1,
+	two: 2,
+};
+
+const newObj = {...q}; 
